0e8fc642878513e9ef1a887c10e8c7af020bb5bb
ebronsted-tcsec <37162476+ebronsted-tcsec@users.noreply.github.com>
2018-03-07 20:01:47 -0500

    moonweight
    worked on moon weight program

--- moon_weight.py
+++ moon_weight.py
@@ -1,8 +1,19 @@
+import sys
 
-def moon_weight(X_e, weight_factor):
-    for years in range(1, 16):
-        X_e = X_e + 1
-        X_m = X_e * weight_factor
-        print("year {}: your weight on the moon is {}".format(years,X_m))
+# def moon_weight(initial_weight, weight_factor, years):
+#     for years in range(1, years + 1):
+#         initial_weight = initial_weight + 1
+#         X_m = initial_weight * weight_factor
+#         print("year {}: your weight on the moon is {}".format(years,X_m))
 
-moon_weight(100, 0.25)
+# moon_weight(90, 0.25, 5)
+
+def moon_weight():
+    initial_weight = int(input("Please enter your current Earth weight"))
+    weight_factor = input("PLease enter the amount your weight might increase each year")
+    years = int(input("Please enter the number of years"))
+    for year in range(1, years + 1):
+        initial_weight = initial_weight + 1
+        X_m = initial_weight * weight_factor
+        print("year {}: your weight on the moon is {}".format(year,X_m))
+moon_weight()
